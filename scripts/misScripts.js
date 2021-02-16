024a38a521664ecdf6ea3f5fd354a289ac362a19
AndresObregon <51458139+AndresObregon@users.noreply.github.com>
2021-02-16 21:39:45 +0100

    First Commit

---
+++ scripts/misScripts.js
@@ -0,0 +1,13 @@
+const input = document.querySelector('input[type="checkbox"]');
+function handleInput() {
+  const { checked } = this;
+  document.she
+}
+input.addEventListener('input', handleInput);
+
+//const input = document.querySelector('input[type="checkbox"]');
+//function handleInput() {
+ // const { checked } = this;
+//  document.querySelector('body').style.background = checked ? '#151d29' : '#d6e7f7';
+//}
+//input.addEventListener('input', handleInput);
